e9b952315802f0f49c24b34d99aefe5ce0b1730c
Facu Guini <facundoguini20@hotmail.com>
2014-10-27 15:04:42 -0300

    gon es gay

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-var app = angular.module('empanadas', ['ngRoute']).config(function($routeProvider){
-    $routeProvider
-        .when('/', {
-            templateUrl: 'templates/main.html',
-            controller: 'MainController'
-        })
+var app = angular.module('empanadas', ['ngRoute']).config(function(){
+
 });
