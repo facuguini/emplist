92e84fae5e55ea51e9fe7c21328622ae47c98429
Facu Guini <facundoguini20@hotmail.com>
2014-10-30 15:47:27 -0300

    los sliders a la puta q lo pario

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -9,27 +9,12 @@ app.controller('mainController', ['$scope', function($scope) {
 
 	if(localStorage.getItem('primeravez')==null) {
 		console.log('entro por primera vez')
-		localStorage.setItem('defecto', '5');
-		localStorage.setItem('n','1');
-		localStorage.setItem('total', '0');
+		localStorage.setItem('defecto', JSON.stringify(5));
 		localStorage.setItem('primeravez', 'false')
-		n = JSON.parse(localStorage.getItem('n'));
-		defecto= JSON.parse(localStorage.getItem('defecto'));
-		Total= JSON.parse(localStorage.getItem('total'))
+		$scope.cantdefecto = JSON.parse(localStorage.getItem('defecto'));
 	}
- 	$( "#defecto" ).val(defecto)
-
-
- 	for (i=2 ; i<=n ; i++) {
- 		$('#contenido').append(JSON.parse(localStorage.getItem('empanada'+i)));
-        $('.ui-page').trigger('create');
- 	}
-
  	$scope.addpre = function(_gusto) {
  		var gusto = _gusto;
-/*	    var content = "<div><label for='empanada" + n + "'>" + gusto + ": </label> <input type='range' class='empanada' name='empanada" + n +"' id='empanada" + n + "' value='"+ defecto + "' min='0' max='50' data-theme='a' /></div>"
-        $('#contenido').append(content);
-        $('.ui-page').trigger('create');*/
         var num;
         if($scope.empanadas!=null){
         	num=$scope.empanadas.length;
@@ -40,18 +25,11 @@ app.controller('mainController', ['$scope', function($scope) {
         var empanada = {
         	name: gusto,
         	id: 'empanada'+num,
-        	value: defecto
+        	value: $scope.cantdefecto
         }
         $scope.empanadas.push(empanada);
-        localStorage.setItem("n",JSON.stringify(n));
         localStorage.setItem('empanadas', JSON.stringify($scope.empanadas));
-        /*localStorage.setItem('empanada'+n, JSON.stringify(content));*/
  	}
-
-
-	//$( "#empanada1" ).bind( "change", function(event, ui) {
-	 //alert($("#empanada1").val());
-	//});
 
 	$( "#defecto" ).on( 'slidestop', function( event ) { 
 		defecto = $("#defecto").val()
@@ -59,21 +37,18 @@ app.controller('mainController', ['$scope', function($scope) {
 	});
 
 
-	$("#boton").click(function( ) { 
-		Total=0;
+	$scope.calcular = function() { 
+		var Total=0;
 	  	$(".empanada").each(function( index ) { 
-	    Total = parseInt(Total) + parseInt($(this).val());
+	    	Total = parseInt(Total) + parseInt($(this).val());
 		}); 
-	console.log(Total);
-	var dtotal = Math.floor(Total/12)
-	console.log(dtotal+" docena(s) y " + (Total-dtotal*12) + " empanadas.")
-	//localStorage.setItem('total', JSON.stringify(Total));
-	});
+		console.log(Total);
+		var dtotal = Math.floor(Total/12)
+		console.log(dtotal+" docena(s) y " + (Total-dtotal*12) + " empanadas.")
+	};
 	var n = JSON.parse(localStorage.getItem('n'));
 	var defecto= JSON.parse(localStorage.getItem('defecto'));
-	var Total=0;
-	//var Total= JSON.parse(localStorage.getItem('total'))
-	//var n=1;
+	
 
 	$scope.addcustom = function() {
 		if($scope.customtext){
@@ -87,26 +62,10 @@ app.controller('mainController', ['$scope', function($scope) {
 	        var empanada = {
 	        	name: $scope.customtext,
 	        	id: 'empanada'+num,
-	        	value: defecto
+	        	value: $scope.cantdefecto
 	        }
 	        $scope.empanadas.push(empanada);
-	        localStorage.setItem("n",JSON.stringify(n));
 	        localStorage.setItem('empanadas', JSON.stringify($scope.empanadas));
         }
 	}
-/*	function runScript(e) {
-	    if (e.keyCode == 13) {
-	        var gusto = $('#fname').val();
-	        if (gusto.length > 0) {
-		        n=n+1;
-		        var content = "<div><label for='empanada" + n + "'>" + gusto + ": </label> <input type='range' class='empanada' name='empanada" + n +"' id='empanada" + n + "' value='"+ defecto + "' min='0' max='50' data-theme='a' /></div>"
-		        $('#contenido').append(content);
-		        $('.ui-page').trigger('create');
-		        localStorage.setItem("n",JSON.stringify(n));
-		        localStorage.setItem('empanada'+n, JSON.stringify(content));
-		   		$('#fname').val('')
-		   		return false;
-	   		}
-	    }
-	}*/
 }]);
